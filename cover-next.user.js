0256eef6c2428a954c5af4e52837548e3f984aeb
akoneko <akoneko47@gmail.com>
2021-08-05 19:28:08 +0700

    fix cache github

--- cover-next.user.js
+++ cover-next.user.js
@@ -16,7 +16,7 @@
 // @match        https://www.siambit.me/details.php*
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
-// @require      https://raw.githubusercontent.com/kon3ko/cover-next/master/dist/cover-next.js
+// @require      https://raw.githubusercontent.com/kon3ko/cover-next/master/dist/cover-next.js?version=1.10
 // @updateURL    https://raw.githubusercontent.com/kon3ko/cover-next/master/cover-next.user.js
 // @downloadURL  https://raw.githubusercontent.com/kon3ko/cover-next/master/cover-next.user.js
 // @supportURL   https://github.com/kon3ko/cover-next/issues
